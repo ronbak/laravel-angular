aef7360e0e960f47d573cca9a5ef011ed9c1fa9d
Raylan <raylan@raylansoares.com>
2016-06-17 00:54:07 -0300

    Frontend - Adicionado autenticação

---
+++ resources/assets/js/controllers/home.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Raylan on 16/06/2016.
+ */
+angular.module('app.controllers')
+    .controller('HomeController',['$scope', function ($scope) {
+        
+    }]);
---
+++ resources/assets/js/controllers/login.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Raylan on 16/06/2016.
+ */
+angular.module('app.controllers')
+.controller('LoginController',['$scope', '$location', 'OAuth', function ($scope, $location, OAuth) {
+    $scope.user = {
+        username: '',
+        password: ''
+    };
+    $scope.error = {
+        message: '',
+        error: false
+    };
+
+    $scope.login = function () {
+        if($scope.formLogin.$valid){
+            OAuth.getAccessToken($scope.user).then(
+                function () {
+                    $location.path('home');
+                },function (data) {
+                    $scope.error.error = true;
+                    $scope.error.message = data.data.error_description;
+                }
+            );
+        }
+    };
+}]);
